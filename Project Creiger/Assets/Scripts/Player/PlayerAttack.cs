666c746c13835e51127faec0ab36d8f686192253
Domynic Desktop <domynicbarroslima@gmail.com>
2025-05-21 16:52:27 -0300

    Reorganiza estrutura de assets, sprites, áudios e atualiza arquivos dp projeto.

--- Project Creiger/Assets/Scripts/Player/PlayerAttack.cs
+++ Project Creiger/Assets/Scripts/Player/PlayerAttack.cs
@@ -1,12 +1,64 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerAttack : MonoBehaviour{ // Script que controla o ataque do Keeper
-    private void OnTriggerStay2D(Collider2D collision){
-        if(collision.CompareTag("Keeper")){
-            collision.GetComponent<KeeperController>().life--; // Diminui a vida do Keeper
+public class PlayerMeleeAttack : MonoBehaviour
+{
+    [Header("Attack Parameters")]
+    [SerializeField] private float attackCooldown = 0.5f;
+    [SerializeField] private float range = 1.0f;
+    [SerializeField] private int damage = 1;
+
+    [Header("Collider Parameters")]
+    [SerializeField] private float colliderDistance = 0.5f;
+    [SerializeField] private BoxCollider2D boxCollider;
+
+    [Header("Enemy Layer")]
+    [SerializeField] private LayerMask enemyLayer;
+
+    private float cooldownTimer = Mathf.Infinity;
+    private Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        cooldownTimer += Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Mouse0) && cooldownTimer >= attackCooldown)
+        {
+            cooldownTimer = 0;
+            anim.SetTrigger("attack");
         }
     }
-}
 
+    // Essa função deve ser chamada via Animation Event no frame certo do ataque
+    private void DamageEnemy()
+    {
+        RaycastHit2D hit = Physics2D.BoxCast(
+            boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
+            new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z),
+            0, Vector2.zero, 0, enemyLayer);
+
+        if (hit.collider != null)
+        {
+            Health enemyHealth = hit.transform.GetComponent<Health>();
+            if (enemyHealth != null)
+            {
+                enemyHealth.TakeDamage(damage);
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (boxCollider == null) return;
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(
+            boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
+            new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z)
+        );
+    }
+}
